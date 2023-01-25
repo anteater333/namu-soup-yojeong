caa66ba92ab67964f2212f79320058666e32abcd
Ji-hoon Lee <anteater1056@gmail.com>
2023-01-25 17:08:37 +0900

    test code 작성

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,18 @@
-import { assertEquals } from "https://deno.land/std@0.172.0/testing/asserts.ts";
-import { add } from "./main.ts";
+import {
+  assertEquals,
+  assertInstanceOf,
+} from "https://deno.land/std@0.172.0/testing/asserts.ts";
+import { getNamuRanking, saveNamuRanking } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(async function 랭킹은_길이가_10인_배열() {
+  const namuRanking = await getNamuRanking();
+  assertInstanceOf(namuRanking, Array);
+
+  assertEquals(namuRanking.length, 10);
+});
+
+Deno.test(async function 랭킹을_서버에_저장하면_201_코드를_받는다() {
+  const expectedResult = 201;
+
+  assertEquals((await saveNamuRanking()).resultCode, expectedResult);
 });
