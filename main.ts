d568d187cdf884e3b8fc6bedf54284113e6cdb4d
Ji-hoon Lee <anteater1056@gmail.com>
2023-01-25 23:43:53 +0900

    get ranking

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export async function getNamuRanking() {
+  const result = await fetch("https://search.namu.wiki/api/ranking", {
+    credentials: "omit",
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0",
+      Accept: "application/json, text/plain, */*",
+      "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-site",
+      // "If-None-Match": 'W/"8f-ifC8pEihCYj9gV0JFs9sPvS4igM"',
+    },
+    referrer: "https://namu.wiki/",
+    method: "GET",
+    mode: "cors",
+  });
+
+  return await result.json();
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
