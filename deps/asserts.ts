555629f67be7b973f8cfa87bc1203c47b1e15086
Ji-hoon Lee <anteater1056@gmail.com>
2023-02-01 23:10:07 +0900

    test - 숲요정에게 명령하기 API 테스트 코드 작성

--- deps/asserts.ts
+++ deps/asserts.ts
@@ -2,4 +2,5 @@ export {
   assertEquals,
   assertInstanceOf,
   assertNotEquals,
+  assertArrayIncludes,
 } from "https://deno.land/std@0.172.0/testing/asserts.ts";
---
+++ hq_test.ts
@@ -0,0 +1,48 @@
+import { orderSoupYojeong, resetCooldown } from "./hq.ts";
+import {
+  assertEquals,
+  assertInstanceOf,
+  assertArrayIncludes,
+} from "./deps/asserts.ts";
+
+const baseurl = "http://localhost:4242";
+const apiTestServer = "http://localhost:8080";
+
+Deno.test(async function 실검_저장_요청이_성공한다() {
+  const req = new Request(baseurl);
+  const res = orderSoupYojeong(req);
+
+  assertEquals(res.status, 200);
+
+  resetCooldown();
+});
+
+Deno.test(async function 요청이_성공하면_저장했던_실검을_반환한다() {
+  const req = new Request(baseurl);
+  const res = orderSoupYojeong(req);
+  const result = await res.json();
+
+  assertInstanceOf(result, Array);
+
+  const apiReq = new Request(`${apiTestServer}/api`);
+  const savedRanking = (
+    await (await fetch(apiReq, { method: "GET" })).json()
+  )[0].map((a: { keyword: string }) => a.keyword);
+
+  assertArrayIncludes(result, savedRanking);
+
+  resetCooldown();
+});
+
+Deno.test(async function 요청을_연속해_보내면_두번째_요청은_실패한다() {
+  const req = new Request(baseurl);
+  const res = orderSoupYojeong(req);
+
+  assertEquals(res.status, 200);
+
+  const res2 = orderSoupYojeong(req);
+
+  assertEquals(res2.status, 429);
+
+  resetCooldown();
+});
